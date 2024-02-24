2eaafb23c497d2e3d9df84e9ecac259d1c32f9b0
Muhammad Mubashir <149854409+mubashir-yaseen@users.noreply.github.com>
2024-02-25 04:37:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,11 @@ def fetch_poster(movie_id):
 
 # Function to recommend similar movies
 def recommend(movie):
-    index = movies.loc[movies['title'] == movie].index[0]
+    selected_movies = movies.loc[movies['title'] == movie]
+    if selected_movies.empty:
+        return []  # Return an empty list if no movie matches the given title
+    
+    index = selected_movies.index[0]
     distances = sorted(enumerate(similarity[index]), reverse=True, key=lambda x: x[1])
     recommended_movies = []
     for i, (movie_index, score) in enumerate(distances[1:6], start=1):
@@ -27,21 +31,24 @@ def recommend(movie):
         })
     return recommended_movies
 
+
 # Load movie data and similarity scores
 with open('movie_list.pkl', 'rb') as file:
     movies = pickle.load(file)
 with open('similarity.pkl', 'rb') as file:
     similarity = pickle.load(file)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    recommended_movies = []
+    if request.method == 'POST':
+        selected_movie = request.form.get('selected_movie')
+        recommended_movies = recommend(selected_movie)
+    # Load the movie list for the dropdown
+    with open('movie_list.pkl', 'rb') as file:
+        movie_list = pickle.load(file)['title'].tolist()
+    return render_template('index.html', recommended_movies=recommended_movies, movie_list=movie_list)
 
-@app.route('/recommendations', methods=['POST'])
-def get_recommendations():
-    selected_movie = request.form.get('selected_movie')
-    recommended_movies = recommend(selected_movie)
-    return render_template('recommendations.html', recommended_movies=recommended_movies)
 
 if __name__ == '__main__':
     app.run(debug=True)
